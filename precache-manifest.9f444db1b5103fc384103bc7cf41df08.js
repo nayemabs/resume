c70b817df89afc55e0668b12c0f95c0f17e2157b
Nayem Abs <vlad@pop-os.localdomain>
2020-02-26 03:48:43 +0600

    Updates

---
+++ precache-manifest.9f444db1b5103fc384103bc7cf41df08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1819043abccc309288932bcdccb1228",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "6ba88201c6b5b19db121",
+    "url": "/resume/static/css/main.15bc5e4a.chunk.css"
+  },
+  {
+    "revision": "d867e28f8370e406d075",
+    "url": "/resume/static/js/2.7b339b6b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/resume/static/js/2.7b339b6b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6ba88201c6b5b19db121",
+    "url": "/resume/static/js/main.749c6e7b.chunk.js"
+  },
+  {
+    "revision": "4a20790a0825df653576",
+    "url": "/resume/static/js/runtime-main.ab232846.js"
+  },
+  {
+    "revision": "ee215ec6328a568415eba03d7563531c",
+    "url": "/resume/static/media/nayem.ee215ec6.jpg"
+  }
+]);
